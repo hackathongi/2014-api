6a5534a4adb75339339ab110f18a2f551dd286bb
casapier <cortsenc@gmail.com>
2014-03-08 23:49:17 +0100

    Feta la API del crawler

--- routes/crawler.js
+++ routes/crawler.js
@@ -17,55 +17,49 @@ module.exports = function(db) {
 				surname		: req.body.USER_SURNAME,
 				email		: req.body.USER_EMAIL
 			});*/
-			//console.log("Parametres: " + req.params);
-			//console.log("Parms+shop: " + req.params.rating);
-			//console.log("Req: " + req);
-			console.log(req.body);
-			var auxLength=req.body.length;
-			console.log(auxLength);
-			for(var aux=0;aux<auxLength;aux++) {
-				console.log(req.body[aux]);
-			}
-			
-			//var foo=JSON.parse(req);
-			//console.log("Foo: " + foo);
-			//console.log("Body+rating: "+req.body.rating);
-			//console.log("Body+shop_name: "+req.body.shop_name);
-			
+
 			var shop = db.Shop.build({
-				name: "aixo es un nom",//req.params.SHOP_NAME,
-				adress: req.params.SHOP_ADDRESS,
-				url: req.params.SHOP_URL,
-				phone : req.params.SHOP_PHONE,
-				email : req.params.SHOP_EMAIL,
-				logo_url : req.params.SHOP_LOGO,
+				name: req.body.shop.shop_name,
+				adress: req.body.shop.shop_address,
+				url: req.body.shop.shop_url,
+				phone : req.body.shop.shop_phone,
+				email : req.body.shop.shop_email,
+				logo_url : req.body.shop.shop_logo,
 				is_client : false,
-				url_scrap : req.params.SHOP_URL_COMPETITOR
+				url_scrap : req.body.shop.shop_url_competitor
 				
 			});
+
+			
 			var order = db.Order.build({
 				description: "CRAWLER",
 				pending: 0
 			});
-			var opinion = db.Opinion.build({
-				description: req.params.DESCRIPTION,
-		        rating: req.params.RATING,
-		        date: req.params.DATE,
-		        lang: req.params.LANGUAGE				
-			});
 			
 			
+			//S'hauria de comprovar que la Shop no existeix
 			shop.save().complete(function(err){
 				if(!err){
 					order.save().complete(function(err){
 						if(!err){
-							opinion.save().complete(function(err){
-								if(!err){
-									res.send(200);
-								}else{
-									res.send(500, { error: 'Error al guardar Opinion' });
-								}
-							});
+							var reviews = req.body.reviews.length;
+							for(var i = 0; i<reviews;i++){
+								var review = req.body.reviews[i];
+								var opinion = db.Opinion.build({
+									description: review.description,
+							        rating: review.rating,
+							        date: review.date,
+							        lang: review.lang				
+								});
+								opinion.save().complete(function(err){
+									if(!err){
+										//res.send(200);
+									}else{
+										res.send(500, { error: 'Error al guardar Opinion' });
+									}
+								});
+							}
+							res.send(200);
 						}else{
 							res.send(500, { error: 'Error al guardar Order' });
 						}
