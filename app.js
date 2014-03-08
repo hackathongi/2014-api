10743981eb27c11e282ae79a19e05b3107938058
Ignacio Martin <natx.mc@gmail.com>
2014-03-08 13:19:51 +0100

    First functional version

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+  , http    = require('http')
+  , path    = require('path')
+ 
+var app = express()
 
-var app = express();
+var db = require('./models')(app)
+
+var routes  = require('./routes')
+
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('port', process.env.PORT || 3000)
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(express.methodOverride())
+app.use(app.router)
+app.use(express.static(path.join(__dirname, 'public')))
+ 
 // development only
 if ('development' === app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(express.errorHandler())
 }
+ 
+app.get('/', routes.index)
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//app.post('/users/create', user.create)
+//app.post('/users/:user_id/tasks/create', task.create)
+//app.get('/users/:user_id/tasks/:task_id/destroy', task.destroy)
