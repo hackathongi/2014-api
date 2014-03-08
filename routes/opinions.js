2f0acf07ffe02f4ed8f1284831211695d9b6f5e7
pauix1992 <pamich1@gmail.com>
2014-03-08 16:45:47 +0100

    Fixed some mistakes

--- routes/opinions.js
+++ routes/opinions.js
@@ -5,21 +5,14 @@
 module.exports = function(db) {
 
 	return {
-	//	list : function(req, res) {
-	//		db.Opinion.findAll({
-	//		// include : [ db.Task ]
-	//		}).success(function(opinions) {
-	//			res.setHeader('Content-Type', 'application/json');
-	//			res.end(JSON.stringify(opinions));
-	//		})
-	//	},
-	//
-		list : function(req,res) {
-			db.Opinion.find({where: {id : req.params.id}})
-				.success(function(opinions){
-					res.setHeader('Content-Type', 'application-json');
-					res.end(JSON.stringify(opinions));
-			});
+		list : function(req, res) {
+			db.Opinion.findAll({
+			// include : [ db.Task ]
+			}).success(function(opinions) {
+				res.setHeader('Content-Type', 'application/json');
+				res.end(JSON.stringify(opinions));
+			})
+
 		},
 		// Revisar que els camps del form es diguin igual que els de la funció
 		add : function(req,res) {
