88df041b5c4b34a1c5f38531c372e5c5ab23461e
Ignacio Martin <natx.mc@gmail.com>
2014-03-08 20:11:40 +0100

    Added uuid support

--- routes/shops.js
+++ routes/shops.js
@@ -9,12 +9,15 @@
 
 module.exports = function(db) {
 
+	var uuid = require('node-uuid');
+	
 	return {
 		add : function(req, res) {
 			var shop = db.Shop.build(req.body);
 			shop.save().complete(function(err) {
 				 if (!err) {
-					 res.send(200);
+					res.setHeader('Content-Type', 'application/json');
+					res.end(JSON.stringify({token: uuid.v4()}));
 				 } else {
 					 res.send(500, { error: err.message });
 				 }
