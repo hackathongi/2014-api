3c0480b32efc6205c857968ef6f69aeb70efc022
Ignacio Martin <natx.mc@gmail.com>
2014-03-19 21:41:30 +0100

    Removed bodyParser() not needed wth json() and urlencoded()

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.use(express.logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.methodOverride())
-app.use(express.bodyParser())
 app.use(cors());
 app.use(app.router)
 app.use(express.static(path.join(__dirname, 'public')))
